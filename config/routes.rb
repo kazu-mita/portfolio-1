9a2a86139be5adc5c5317cdfc0d72a134553d40f
github kazu-mita <kazu0726m@icloud.com>
2020-09-11 02:22:38 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  resources :users
+  resources :users, only: %i[index]
   get  'results/first' => 'results#first'
   get  'results/second' => 'results#second'
   get  'results/third' => 'results#third'
-  resources :hospitals
+  resources :hospitals, only: %i[index show]
 end
